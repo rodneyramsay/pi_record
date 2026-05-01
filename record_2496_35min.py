d0564e9d15d89c82fc0e3824150d8ad71be7b749
Rodney Ramsay <rjrodney@gmail.com>
2026-04-30 20:21:12 -0600

    Refactor sox command for better readability

--- record_2496_35min.py
+++ record_2496_35min.py
@@ -55,8 +55,14 @@ def main():
 
 
     cmd = [
-        "sox", output_wav, "-p", "trim", "150", "|", 
-        "sox", "-p", "-n", "stats"
+        "sox", output_wav, 
+        "-p", 
+        "trim", "150", 
+        "|", 
+        "sox", 
+        "-p", 
+        "-n", 
+        "stats"
     ]
 
     print(*cmd)
